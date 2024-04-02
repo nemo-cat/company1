e2ffb1b57b49bb5d0041b3977b493646de0ccbee
sori <qjxj1112@naver.com>
2024-04-02 16:49:40 +0900

    시작

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function()
+{
+    let mySwiper = new Swiper('.swiper-container',
+    {
+        direction: 'horizontal',
+        loop: true,
+        autoplay:
+        {
+            delay: 3000
+        },
+        speed: 300
+
+      });
+})
