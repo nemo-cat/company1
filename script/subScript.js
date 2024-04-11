c25c35127bd43f02dd58e138da72cea46abddd4b
sori <qjxj1112@naver.com>
2024-04-11 17:04:58 +0900

    서브5 작성중

--- script/subScript.js
+++ script/subScript.js
@@ -16,4 +16,25 @@ $(document).ready(function()
         $('.history-list').hide()
         $('.history-list.active').show()
     })
+
+    /*
+    let newsItem = $('.news ul').children()으로 해서
+    append(newsItem)하면 아무런 반응이 없음.
+    알고보니 $('.news ul').children()으로만 하면 원본 요소를 $('#newsList')으로 이동하는게 되어서
+    변화가 없으니 반응이 없는것처럼 보이는것임.
+    요소를 복제해서 추가해야 원하는대로 동작함!!
+    */
+    let newsItem = $('#newsList').children();
+
+    $('.news-more').click(function()
+    {
+       $('#newsList').append(newsItem.clone());
+    })
+
+    let promotionItem = $('#promotionList').children();
+
+    $('.promotion-more').click(function()
+    {
+       $('#promotionList').append(promotionItem.clone());
+    })
 });
